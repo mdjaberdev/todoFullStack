0d67a2ee649417e20d1aaebddde5c14c5808926d
mdjaberdev <md9292463@gmail.com>
2026-06-24 23:41:30 +0600

    condidation

--- backend/controllers/todoController.js
+++ backend/controllers/todoController.js
@@ -1,40 +1,23 @@
 const Todo = require("../models/userModel");
-const todoController = (req, res) => {
+const todoController = async (req, res) => {
   const { task, status, priority } = req.body;
   console.log(task);
-
   if (!task || !priority) {
     return res.send({
       success: false,
-      message: "Please fill the all fields",
-    });
-    const newTodo = new Todo({
-      task: task,
-      priority: priority,
-    });
-    newTodo.save();
-    res.send({
-      success: true,
-      message: "Task add",
-    });
-    if (!task || !priority) {
-      return res.send({
-        success: false,
-        message: "Please fill all the field",
-      });
-    }
-
-    const todo = new Todo({
-      task: task,
-      priority: priority,
-    });
-
-    todo.save();
-    res.send({
-      success: true,
-      message: "Todo created",
+      message: "Please Fil the all fields",
     });
   }
+
+  const newTodo = new Todo({
+    task: task,
+    priority: priority,
+  });
+  await  newTodo.save();
+  res.send({
+    success: true,
+    message: "Task  add",
+  });
 };
 
 module.exports = todoController;
